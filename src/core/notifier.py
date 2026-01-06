17cbdad609914ff1a775aedd3785e8a8d21ef258
mi6gun <endlesspossibilitieskr@gmail.com>
2026-01-06 15:48:52 +0500

    Метод по уведомлению пользователя(админа) о состоянии бота

---
+++ src/core/notifier.py
@@ -0,0 +1,17 @@
+from aiogram import Bot
+from aiogram.exceptions import TelegramForbiddenError
+
+
+class Notifier:
+    __bot: Bot
+
+    @classmethod
+    async def setup(cls, bot: Bot) -> None:
+        cls.__bot = bot
+
+    @classmethod
+    async def notify(cls, chat_id: int, text: str) -> None:
+        try:
+            await cls.__bot.send_message(chat_id, text)
+        except TelegramForbiddenError:
+            print(f"Пользователь с таким id: {chat_id} заблокировал бота")
